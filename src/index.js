3d8ee21ae16467c2ea013c3bcb333380c2d18ea9
sooman992 <fm01793176380@gmail.com>
2025-06-29 03:42:47 +0600

    few issue fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import ReactDOM from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
